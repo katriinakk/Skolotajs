2f70d2768e7c7691385aa3edcce1b1af5b20a02a
katriinakk <123467252+katriinakk@users.noreply.github.com>
2024-09-13 18:59:32 +0300

    yt
    g

--- skolotajs2.py
+++ skolotajs2.py
@@ -1,22 +1,19 @@
-from winreg import KEY_ALL_ACCESS
-
-
 class Skolotajs:
     def __init__(self, sk_tips, uzvards):
         self.sk_tips = sk_tips
         self.uzvards = uzvards
     
 class SakumskolasSkolotajs(Skolotajs):
-    def __init__(self, klase, uzvards, st_sk, sk_tips = 1):
-        super().__init__(sk_tips, uzvards)
+    def __init__(self, uzvards, klase, st_sk):
+        super().__init__(uzvards)
         self.klase = klase
         self.st_sk = st_sk
 
-    def izd(uzvards, klase, st_sk, sk_tips = "sākumskolas skolotājs"):
-        return("Tips: {},\n Uzvārds: {},\n Pasniegto stundu skaits: {}\nKlase: {}").format(sk_tips, uzvards, st_sk, klase)
+    def izd(klase, uzvards, st_sk):
+        return(" Tips: sākumskolas skolotājs,\n Uzvārds: {},\n Pasniegto stundu skaits: {}\n Klase: {}").format(uzvards, st_sk, klase)
 
 class VidusskolasSkolotajs(Skolotajs):
-    def __init__(self, pr1, pr2, pr1n, pr2n, uzvards, sk_tips = 3):
+    def __init__(self, pr1, pr2, pr1n, pr2n, uzvards, sk_tips):
         super().__init__(uzvards, sk_tips)
         self.pr1 = pr1
         self.pr2 = pr2
@@ -24,14 +21,24 @@ class VidusskolasSkolotajs(Skolotajs):
         self.pr2n = pr2n
 
     def izdruka2(uzvards, pr1n, pr2n, sk_tips, pr1, pr2):
-        return("Tips: {},\nUzvārds: {},\n1. Priekšmeta nosaukums un skaits: {}, {}(sk.)\n2. Priekšmeta nosaukums un skaits: {}, {}(sk.)\nPasniegto stundu skaits: {}").format(sk_tips, uzvards, pr1n, pr1, pr2n, pr2, pr1n+pr2n)
+        return(" Tips: {},\nUzvārds: {},\n1. Priekšmeta nosaukums un skaits: {}, {}(sk.)\n2. Priekšmeta nosaukums un skaits: {}, {}(sk.)\nPasniegto stundu skaits: {}").format(sk_tips, uzvards, pr1n, pr1, pr2n, pr2, pr1n+pr2n)
 
-tipi = input("Ievadiet skolotāja tipu: 1- sākumskolas skolotājs, 3 - vidusskolas skolotājs -> ")
-uzv = input("Ievadiet skolotāja uzvārdu -> ")
+tipi = int(input("Ievadiet skolotāja tipu: 1- sākumskolas skolotājs, 3 - vidusskolas skolotājs -> "))
+uzv = str(input("Ievadiet skolotāja uzvārdu -> "))
+skol = Skolotajs(tipi, uzv)
 
 if (tipi == 1):
     stundu_sk = input("Ievadiet skolotāja stundu skaitu -> ")
     klasite = input("Ievadiet skolotāja klasi -> ")
 
-    skol = SakumskolasSkolotajs(klasite, uzv, stundu_sk, "sākumskolas skolotājs")
-    print(skol.izd())
+    skol = SakumskolasSkolotajs(uzv, klasite, stundu_sk)  
+    print(skol.izd(uzv, stundu_sk, skol.klase))
+
+    
+#else:
+ #   print("error")
+
+print(skol.uzvards)
+print(skol.sk_tips)
+print(skol.st_sk)
+print(skol.klase)
